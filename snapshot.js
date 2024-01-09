b0838061cb5c27262e15bc73fb1c40952dd33f17
Bill Chan <billypchan@gmail.com>
2024-01-09 13:10:06 +0100

    Update snapshot.js

--- snapshot.js
+++ snapshot.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+  await page.goto('https://play.google.com/store/apps/details?id=app.florio.itp&hl=en&gl=US&pli=1');
   await page.screenshot({ path: 'snapshot.png' });
   await browser.close();
 })();
