13d4bdabd9666cf146c966cadbfe0f9359cc2465
Bill Chan <billypchan@gmail.com>
2024-01-09 12:55:46 +0100

    Create snapshot.js

---
+++ snapshot.js
@@ -0,0 +1,10 @@
+// snapshot.js
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  await page.screenshot({ path: 'snapshot.png' });
+  await browser.close();
+})();
