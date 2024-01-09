4dcfec51de59911c561756d2f590832b033efed7
Bill Chan <billypchan@gmail.com>
2024-01-09 13:13:35 +0100

    Update snapshot.js

--- snapshot.js
+++ snapshot.js
@@ -4,7 +4,17 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+
+  // Set the viewport size to cover the entire page height
+  await page.setViewport({
+    width: 1200, // Adjust the width as needed
+    height: await page.evaluate(() => document.body.clientHeight),
+  });
+
   await page.goto('https://play.google.com/store/apps/details?id=app.florio.itp&hl=en&gl=US&pli=1');
-  await page.screenshot({ path: 'snapshot.png' });
+
+  // Take a screenshot of the entire page
+  await page.screenshot({ path: 'snapshot.png', fullPage: true });
+
   await browser.close();
 })();
